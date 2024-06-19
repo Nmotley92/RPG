a1fe229c3acf919e6b46ea74fc39d0574ced9ade
Nathaniel Motley <Nmotley92@gmail.com>
2024-06-18 22:02:08 -0500

    Adds else to buy weapon fucntion in case player doesn't have enough gold

--- script.js
+++ script.js
@@ -89,8 +89,13 @@ function buyWeapon() {
   if (gold >= 30) {
     gold -= 30;
     currentWeaponIndex++;
-    goldText.innerText=gold;
-    text.innerText ="You now have a new weapon."
+    goldText.innerText = gold;
+    let newWeapon = weapons[currentWeaponIndex].name;
+    text.innerText = "You now have a " + newWeapon + ".";
+    inventory.push(newWeapon);
+    text.innerText += " In your inventory you have: " + inventory;
+  } else {
+    text.innerText = "You do not have enough gold to buy a weapon.";
   }
 }
 
